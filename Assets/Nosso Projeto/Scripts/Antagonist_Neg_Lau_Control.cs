af5d3cc36e3eac2aa4b9864fd2eaa8e0e414ef2f
Leandromor0 <113152803+Leandromor0@users.noreply.github.com>
2022-09-13 07:13:33 -0300

    Commit Quinto

---
+++ Assets/Nosso Projeto/Scripts/Antagonist_Neg_Lau_Control.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+
+public class Antagonist_Neg_Lau_Control : MonoBehaviour
+{
+
+    //o SerializeField e para a opção aparecer no menu de modificação
+    [SerializeField] Transform Protagonist_Lau;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float Speed;
+
+    //Criação de Variáveis
+    //o Vetor 2 é uma variável que consegue ter dois valores
+    
+    Vector2 movement;
+
+
+    [SerializeField] float Vision_reach;
+    [SerializeField] float Attack_range;
+
+
+    //variaveis para o ataque e o tempo 
+    [SerializeField] float Attack_Speed;
+    private float Nex_Attack = 0f ;
+
+
+
+    private void Update()
+    {
+        
+        float _Protagonist_Lau_Distance = Vector2.Distance(transform.position, Protagonist_Lau.position);
+
+        //O IF serve para alterar o fluxo de execução, se  verdadeiro faz se for  falso não faz
+       
+        //se o player estive na visão então
+        if (_Protagonist_Lau_Distance <= Vision_reach)
+        {
+
+            Debug.Log("Neg Lau pode ver o lau ");
+            //então ele pode atacar 
+            if(_Protagonist_Lau_Distance <= Attack_range)
+            {
+                Debug.Log("Neg Lau pode atacar o lau ");
+                Attack_Protagonist_Lau();
+
+            }
+            //então ele não pode atacar e vai perseguir 
+            else
+            {
+
+                Debug.Log("Neg Lau vai Perseguir o lau ");
+
+                Chase_Protagonist_Lau(); 
+
+            }
+
+        }
+        //se o player Não estive na visão então
+        else
+        {
+
+            Debug.Log("Neg Lau não pode ver o lau ");
+
+            movement = Vector2.zero;
+
+
+        }
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        //usando RB que criamos la em cima podemos ter movimento 
+        rb.velocity = movement * Speed;
+
+        
+        
+
+    }
+
+
+
+
+    //criar o ataque 
+
+    void Attack_Protagonist_Lau() 
+    {
+
+        movement= Vector2.zero;
+
+        if (Time.time >= Nex_Attack)
+        {
+            // colocar tempo para o proximo ataque 
+            Nex_Attack = Time.time + (1f / Attack_Speed);
+            Debug.Log("ataque ativado");
+
+
+        }
+
+        else 
+        {
+        
+        }
+
+
+    }
+
+
+
+    //presequir o jogador 
+
+    void Chase_Protagonist_Lau()
+    {
+        //criar uma variavel privada so para ser usada aqui ajuda a otimizar o jogo 
+        Vector2 Direction =  Protagonist_Lau.position - transform.position;
+
+        Direction = Direction.normalized;
+
+        movement = Direction;
+
+
+    }
+
+
+
+}
